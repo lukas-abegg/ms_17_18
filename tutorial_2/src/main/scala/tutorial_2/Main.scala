cc6ad44980e3d41ce80cb2eed9ede203a5fd243d
Lukas Abegg <lukas.abegg1@gmail.com>
2017-11-30 02:34:26 +0100

    changes in tut2 logic

--- tutorial_2/src/main/scala/tutorial_2/Main.scala
+++ tutorial_2/src/main/scala/tutorial_2/Main.scala
@@ -5,17 +5,23 @@ import java.nio.file.Files
 import java.nio.charset.StandardCharsets._
 
 import scala.collection.JavaConverters._
-import scala.collection.mutable.ListBuffer
 import scala.util.matching.Regex
 import scalaz.Scalaz._
 
 
 object Main extends App {
 
-  Helper.parse("/home/lukas/git-projects/ms_2017_18/tutorial_2/brown_training")
+  PosTagger.fit("/Users/lukas/git-projects/ms_2017_18/tutorial_2/brown_training/test")
 }
 
-object Helper {
+case class Trigram(pos1: String, pos2: String, pos3: String)
+
+object PosTagger {
+
+  private var trainedWords: Map[Trigram, Int] = null
+  private var trainedPOSTags: Map[Trigram, Int] = null
+
+  private var predWords: Map[Trigram, Int] = null
 
   private def getListOfFiles(directory: String): List[String] = {
     val dir = FileSystems.getDefault.getPath(directory)
@@ -25,32 +31,32 @@ object Helper {
       .toList.map(s => new String(Files.readAllBytes(s), UTF_8))
   }
 
-  def mergeMap(results: List[Map[String, Map[String, String]]]): Map[String, Map[String, String]] =
-    results.reduceLeft { (m, x) =>
-      m |+| x
+  private def buildTriple(elements: Iterator[List[String]]) =
+    elements.foldLeft(Map.empty[Trigram, Int]) { (m, x) =>
+      val trigram = Trigram(x.head, x(1), x.last)
+      m + ((trigram, m.getOrElse(trigram, 0) + 1))
     }
 
-  private def addElements(elements: Iterator[List[String]]): Map[String, Map[String, String]] =
-    elements.foldLeft(Map.empty[String, Map[String, String]]) { (m, x) =>
-      m + ((x.head, m.getOrElse(x.head, Map.empty[String, String]) + (x(1) -> x(2))))
+  private def getTriple(r: Regex, lines: List[String]): Map[Trigram, Int] =
+    lines.foldLeft(Map.empty[Trigram, Int]) { (m, line) =>
+      m |+| buildTriple(tokenizeLine(r, line).sliding(3))
     }
 
-  private def getTriple(r: Regex, lines: List[String]): List[(String, String, String)] =
-    lines.flatMap { line =>
-      (List("$") ++ r.findAllIn(line).toList ++ List("$")).sliding(3).map { t =>
-        (t.head, t(2), t.last)
-      }
-    }
+  private def tokenizeLine(r: Regex, line: String): List[String]  =
+    (List("$") ++ r.findAllIn(line).toList ++ List("$"))
 
-  def parse(dir: String) = {
-    val lines = getListOfFiles(dir).flatMap(splitSentences.findAllIn(_))
+  private lazy val splitSentences: Regex = "[\\t](.+)*".r
+  private lazy val getWords: Regex = "\\S+(?=\\/\\S+)".r
+  private lazy val getPOSs: Regex = "(?<=\\S\\/)\\S+".r
 
-    val words = getTriple(getWords, lines)
-    val posTags = getTriple(getPOSs, lines)
+  def fit(dir: String) = {
+    val lines = getListOfFiles(dir).take(10).flatMap(splitSentences.findAllIn(_))
+    this.trainedWords = getTriple(getWords, lines)
+    this.trainedPOSTags = getTriple(getPOSs, lines)
   }
 
-  lazy val splitSentences: Regex = "[\\t](.+)*".r
-
-  lazy val getWords: Regex = "\\S+(?=\\/\\S+)".r
-  lazy val getPOSs: Regex = "(?<=\\S\\/)\\S+".r
+  def pred(dir: String) = {
+    val lines = getListOfFiles(dir).take(10).flatMap(splitSentences.findAllIn(_))
+    this.predWords = getTriple(getWords, lines)
+  }
 }
