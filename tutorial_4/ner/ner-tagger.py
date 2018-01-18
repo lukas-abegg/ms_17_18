b4ea060ff46829f37919471e76a9d740d04546b9
Lukas Abegg <lukas.abegg@springer.com>
2018-01-18 21:46:29 +0100

    refactoring tagger

--- tutorial_4/ner/ner-tagger.py
+++ tutorial_4/ner/ner-tagger.py
@@ -9,25 +9,27 @@ import pickle
 
 import nltk
 from nltk import SnowballStemmer
-from nltk import edit_distance
+from nltk import edit_distance, pos_tag
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 
 
 class Token:
-    def __init__(self, value, tag):
+    def __init__(self, value: str, tag: str, pos_tagging: str):
         self.value = value
         self.tag = tag
+        self.pos_tagging = pos_tagging
 
 
 class NERTagger:
     __tag_no_entity = "O"
 
     def __init__(self, *args):
-        if len(args) == 3:
+        if len(args) == 4:
             self.dictionary = args[0]
             self.stopwords_list = args[1]
             self.punctuation = args[2]
+            self.pos_tagging = args[3]
         else:
             self.__load_dictionaries()
 
@@ -35,20 +37,22 @@ class NERTagger:
         pickle.dump(self.dictionary, open("dictionary.p", "wb"))
         pickle.dump(self.stopwords_list, open("stopwords_list.p", "wb"))
         pickle.dump(self.punctuation, open("punctuation.p", "wb"))
+        pickle.dump(self.pos_tagging, open("pos_tagging.p", "wb"))
 
     def __load_dictionaries(self):
         self.dictionary = pickle.load(open("dictionary.p", "rb"))
         self.stopwords_list = pickle.load(open("stopwords_list.p", "rb"))
         self.punctuation = pickle.load(open("punctuation.p", "rb"))
+        self.pos_tagging = pickle.load(open("pos_tagging.p", "rb"))
 
     def tag_tokens(self, words: List[Token]):
         tagged_words = []
         for word in words:
             if self.__check_no_entity(word):
-                tagged_words.append(Token(word.value, self.__tag_no_entity))
+                tagged_words.append(Token(word.value, self.__tag_no_entity, word.pos_tagging))
             else:
                 tag = self.__check_rules(word)
-                tagged_words.append(Token(word.value, tag))
+                tagged_words.append(Token(word.value, tag, word.pos_tagging))
         return tagged_words
 
     def __check_no_entity(self, word: Token) -> bool:
@@ -62,17 +66,18 @@ class NERTagger:
     def __check_rules(self, word: Token) -> str:
         keys = list(self.dictionary.keys())
 
-        if word in self.dictionary:
-            return self.dictionary[word].key
+        if word.pos_tagging in self.pos_tagging:
+            if word.value in keys:
+                return self.dictionary[word.value]
+            else:
+                found, tag = self.__check_distance(word, keys)
+                # if found:
+                #     return tag
+                # else:
+                #     found, tag = self.__check_word_stems(word, keys)
         else:
-            found, tag = self.__check_distance(word, keys)
-            return tag
-            # if found:
-            # return tag
-            # else:
-            # found, tag = self.__check_word_stems(word, keys)
-            # return tag
-        # elif for further rules
+            tag = self.__tag_no_entity
+        return tag
 
     def __check_distance(self, word: Token, keys) -> (bool, str):
         # calculate edit-distances
@@ -86,7 +91,7 @@ class NERTagger:
         else:
             return False, self.__tag_no_entity
 
-    def __check_word_stems(self, word, keys):
+    def __check_word_stems(self, word, keys) -> (bool, str):
         stemmer = SnowballStemmer("english")
         stemmed_word = stemmer.stem(word.value)
         stemmed_keys = list(map(lambda x: stemmer.stem(x), keys))
@@ -95,7 +100,7 @@ class NERTagger:
             idx = stemmed_keys.index(stemmed_word)
             return True, self.dictionary[keys[idx]]
 
-        return False
+        return False, self.__tag_no_entity
 
 
 def main():
@@ -114,12 +119,12 @@ def main():
 
 
 def train_tagger(input_file):
-    gold_std = build_dict_from_input_file(input_file)
+    gold_std, pos_taggings = build_dict_from_input_file(input_file)
     additional_dictionary = build_additional_dict()
     dictionary = concatenate_dicts(gold_std, additional_dictionary)
     stopwords_list = build_stopwords()
     punctuation = set(string.punctuation)
-    tagger = NERTagger(dictionary, stopwords_list, punctuation)
+    tagger = NERTagger(dictionary, stopwords_list, punctuation, pos_taggings)
     tagger.save_dictionaries()
 
 
@@ -130,13 +135,15 @@ def tag_tokens(input_file, output_file):
     write_annotations_to_file(tagged_list, output_file)
 
 
-def build_dict_from_input_file(path):
+def build_dict_from_input_file(path) -> (dict, set):
     entities_from_file = {}
+    pos_taggings_from_file = []
     with open(path, "r", encoding='latin-1') as f:
         for token in get_tokens_from_input_file(f.readlines()):
             if token.tag != "O":
                 entities_from_file[token.value] = token.tag
-    return entities_from_file
+                pos_taggings_from_file.append(token.pos_tagging)
+    return entities_from_file, set(pos_taggings_from_file)
 
 
 def build_additional_dict():
@@ -163,10 +170,8 @@ def build_stopwords():
 
 
 def read_tokens_from_input_file(path):
-    tokens = []
     with open(path, "r", encoding='latin-1') as f:
-        for token in get_tokens_from_input_file(f.readlines()):
-            tokens.append(token)
+        tokens = get_tokens_from_input_file(f.readlines())
     return tokens
 
 
@@ -187,13 +192,24 @@ def get_tokens_from_list(lines):
     return tokens
 
 
-def get_tokens_from_input_file(lines):
+def get_tokens_from_input_file(lines) -> List[Token]:
+    words = []
+    tags = []
+    pos_taggings = []
     tokens = []
+    # reading words / tags
     for line in lines:
         if len(line) > 0:
             found_tokens = word_tokenize(line)
             if len(found_tokens) == 2:
-                tokens.append(Token(found_tokens[0], found_tokens[1]))
+                words.append(found_tokens[0])
+                tags.append(found_tokens[1])
+    # reading pos-tags
+    for pos_tagging in pos_tag(words):
+        pos_taggings.append(pos_tagging[1])
+    # concatenate all
+    for i in range(len(words)):
+        tokens.append(Token(words[i], tags[i], pos_taggings[i]))
     return tokens
 
 
