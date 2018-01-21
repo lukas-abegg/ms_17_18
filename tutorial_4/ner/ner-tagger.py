3a27a9897ecd554140055a0b5c799f69233628a3
Lukas Abegg <lukas.abegg1@gmail.com>
2018-01-21 18:47:10 +0100

    final version

--- tutorial_4/ner/ner-tagger.py
+++ tutorial_4/ner/ner-tagger.py
@@ -6,10 +6,11 @@ import math
 from typing import List
 
 import pickle
+import numpy as np
 
 import nltk
 from nltk import SnowballStemmer
-from nltk import edit_distance, jaccard_distance, pos_tag
+from nltk import edit_distance, pos_tag
 from nltk.util import ngrams
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
@@ -46,14 +47,19 @@ class NERTagger:
         self.punctuation = pickle.load(open("punctuation.p", "rb"))
         self.pos_tagging = pickle.load(open("pos_tagging.p", "rb"))
 
-    def tag_tokens(self, words: List[Token]):
+    def tag_tokens(self, words: List[Token]) -> List[Token]:
         tagged_words = []
+        i = 0
+        print("words to tag: {}".format(str(len(words))))
         for word in words:
             if self.__check_no_entity(word):
                 tagged_words.append(Token(word.value, self.__tag_no_entity, word.pos_tagging))
             else:
                 tag = self.__check_rules(word)
                 tagged_words.append(Token(word.value, tag, word.pos_tagging))
+            i += 1
+            if i % 1000 == 0:
+                print(i)
         return tagged_words
 
     def __check_no_entity(self, word: Token) -> bool:
@@ -71,53 +77,77 @@ class NERTagger:
             if word.value in keys:
                 return self.dictionary[word.value]
             else:
-                found, tag = self.__check_distance(word, keys)
-                # if found:
-                #     return tag
-                # else:
-                #     found, tag = self.__check_word_stems(word, keys)
+                tag = self.__tag_no_entity
+            # found, tag = self.__check_distance(word, keys)
+            # if found:
+            #     return tag
+            # else:
+            #     found, tag = self.__check_word_stems(word, keys)
+
         else:
             tag = self.__tag_no_entity
         return tag
 
-    def __check_distance(self, word: Token, keys) -> (bool, str):
-        # distance, index_min = self.__min_edit_distance(word, keys)
-        distance, index_min = self.__min_jaccard_distance(word, keys, 3)
+
+def __check_distance(self, word: Token, keys) -> (bool, str):
+    # calculate edit-distances
+    relevant_keys = [k for k in keys if np.abs(len(k) - len(word.value)) < 4]
+
+    if len(word.value) < 4:
+        distance, index = self.__min_edit_distance(word, relevant_keys)
 
         if distance < round(math.floor(len(word.value)) * 0.9):
-            return True, self.dictionary[keys[index_min]]
+            return True, self.dictionary[relevant_keys[index]]
+        else:
+            return False, self.__tag_no_entity
+    else:
+        similarity, index = self.__max_jaccard_similarity(word, relevant_keys, 3)
+
+        if similarity > 0.8:
+            return True, self.dictionary[relevant_keys[index]]
         else:
             return False, self.__tag_no_entity
 
-    @staticmethod
-    def __min_edit_distance(word: Token, keys):
-        # calculate edit-distances
-        distances = list(map(lambda x: edit_distance(word.value, x), keys))
-        # get index where distance is minimal - conflict resolution happens here as we just pick first index_min
-        index_min = min(range(len(distances)), key=distances.__getitem__)
-        return distances[index_min], index_min
 
-    @staticmethod
-    def __min_jaccard_distance(word: Token, keys, gram_number):
-        # calculate edit-distances
-        distances = ((jaccard_distance(set(ngrams(word.value, gram_number)),
-                                       set(ngrams(key, gram_number))), key)
-                     for key in keys)
-        # get index where distance is minimal - conflict resolution happens here as we just pick first index_min
-        closest = min(distances)
+@staticmethod
+def __min_edit_distance(word: Token, keys) -> (float, int):
+    # calculate edit-distances
+    distances = list(map(lambda x: edit_distance(word.value, x), keys))
+    # get index where distance is minimal - conflict resolution happens here as we just pick first index_min
+    index_min = min(range(len(distances)), key=distances.__getitem__)
+    return distances[index_min], index_min
 
-        return closest[0], keys.index(closest[1])
 
-    def __check_word_stems(self, word, keys) -> (bool, str):
-        stemmer = SnowballStemmer("english")
-        stemmed_word = stemmer.stem(word.value)
-        stemmed_keys = list(map(lambda x: stemmer.stem(x), keys))
+@staticmethod
+def __max_jaccard_similarity(word: Token, keys, gram_number) -> (float, int):
+    # calculate jaccard-distances
+    distances = list(map(lambda x: jaccard_similarity(set(ngrams(word.value, gram_number)),
+                                                      set(ngrams(x, gram_number))), keys))
+    index_max = max(range(len(distances)), key=distances.__getitem__)
+    return distances[index_max], index_max
 
-        if stemmed_word in stemmed_keys:
-            idx = stemmed_keys.index(stemmed_word)
-            return True, self.dictionary[keys[idx]]
 
-        return False, self.__tag_no_entity
+def __check_word_stems(self, word, keys) -> (bool, str):
+    stemmer = SnowballStemmer("english")
+    stemmed_word = stemmer.stem(word.value)
+    stemmed_keys = list(map(lambda x: stemmer.stem(x), keys))
+
+    if stemmed_word in stemmed_keys:
+        idx = stemmed_keys.index(stemmed_word)
+        return True, self.dictionary[keys[idx]]
+
+    return False, self.__tag_no_entity
+
+
+def jaccard_similarity(label1: set, label2: set) -> float:
+    intersection_cardinality = (len(label1.union(label2)) - len(label1.intersection(label2)))
+    union_cardinality = len(label1.union(label2))
+
+    if intersection_cardinality > 0 and union_cardinality > 0:
+        result = intersection_cardinality / float(union_cardinality)
+    else:
+        result = 0
+    return result
 
 
 def main():
@@ -137,7 +167,7 @@ def main():
 
 def train_tagger(input_file):
     gold_std, pos_taggings = build_dict_from_input_file(input_file)
-    additional_dictionary = build_additional_dict()
+    additional_dictionary = build_additional_dict("./../data/dictionary/human-genenames.txt")
     dictionary = concatenate_dicts(gold_std, additional_dictionary)
     stopwords_list = build_stopwords()
     punctuation = set(string.punctuation)
@@ -163,20 +193,22 @@ def build_dict_from_input_file(path) -> (dict, set):
     return entities_from_file, set(pos_taggings_from_file)
 
 
-def build_additional_dict():
+def build_additional_dict(path: str) -> dict:
     entities_from_file = {}
-    path = "./../data/dictionary/human-genenames.txt"
     with open(path, "r", encoding='latin-1') as f:
         for token in get_tokens_from_list(f.readlines()):
             entities_from_file[token] = "B-protein"
     return entities_from_file
 
 
-def concatenate_dicts(gold_std, additional_dict):
-    return dict(list(gold_std.items()) + list(additional_dict.items()))
+def concatenate_dicts(gold_std, additional_dict) -> dict:
+    set1 = set(list(gold_std.items()))
+    set2 = set(list(additional_dict.items()))
+    set_conc = set1.union(set2)
+    return dict(set_conc)
 
 
-def build_stopwords():
+def build_stopwords() -> set:
     nltk.download('stopwords')
     stopwords_list = []
     path = "./../data/dictionary/english_stop_words.txt"
@@ -186,7 +218,7 @@ def build_stopwords():
     return set(stopwords_list).union(set(stopwords.words('english')))
 
 
-def read_tokens_from_input_file(path):
+def read_tokens_from_input_file(path) -> List[Token]:
     with open(path, "r", encoding='latin-1') as f:
         tokens = get_tokens_from_input_file(f.readlines())
     return tokens
@@ -199,7 +231,7 @@ def write_annotations_to_file(annotations: List[Token], output_file):
     my_file.close()
 
 
-def get_tokens_from_list(lines):
+def get_tokens_from_list(lines) -> list:
     tokens = []
     for line in lines:
         if len(line) > 0:
